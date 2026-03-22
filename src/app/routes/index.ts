46e103b54032a1281e58635ce209f6b27c05bdfa
iktushar01 <iktushar2020@gmail.com>
2026-03-22 21:18:42 +0600

    feat: implement classroom module with dedicated routes, controllers, services, validation, and middleware.

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { AuthRoute } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
+import { ClassroomRoutes } from "../module/classrooom/classroom.route";
 const router = express.Router();
 
 router.use("/auth", AuthRoute);
 router.use("/users", UserRoutes);
+router.use("/classrooms", ClassroomRoutes);
 
 
 export const IndexRoute = router;
